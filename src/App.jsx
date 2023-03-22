1c5669372af324935fba4e1345c9be42aee71422
Diego Gutierrez <diego_ggv@pm.me>
2023-03-22 12:54:24 -0400

    add basic firebase functionality and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
+//  Firebase
+//==========
+import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
+import {getDatabase, ref, push} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
+
 import {useState} from 'react'
 import {nanoid} from 'nanoid';
-import './css/App.css'
 import Item from './components/item.jsx';
+import './css/App.css'
 
 /*
  Todo: 
-   - add buttons that add or subtract how of each item to get 
+   - add buttons that add or subtract how of each item to get
+   - add a check box to each li item and when checked cross the li items
+    from the lis
+   - create a button that removes all items from the list 
  DONE - create a function that adds the current input's value to the item list
  DONE - create a button that removes or crosses the items once purchased
  */
 
+
+
 function App() {
+  //  Firebase 
+  //==========
+  const appSettings = {
+    databaseURL: 'https://playground-e9327-default-rtdb.firebaseio.com/'
+  }
+  const app = initializeApp(appSettings)
+  const database = getDatabase(app)
+  const shoppingCartDB = ref(database, 'shoppingList')
+  
+  //  State
+  //=======
   const [itemList, setSetItemList] = useState([])
   const [currentItem, setCurrentItem] = useState({
     id: '',
     name: '',
   })
-
+  
+  //  Functions
+  //===========
   function newItem(e) {
     setCurrentItem({
       id: nanoid(),
@@ -28,6 +51,7 @@ function App() {
     if (currentItem.name !== '') {
       setSetItemList(prevState => [currentItem, ...prevState])
       setCurrentItem({ di: '', name: '',})
+      push(shoppingCartDB, currentItem)
     }
   }
  
@@ -48,13 +72,18 @@ function App() {
   return (
       <div className="App">
         <h1 className="title">Shopping List</h1>
-        <input
+
+        <div className="container">
+          <input
             className="input--field"
             placeholder="add item to the list"
             name="input"
             value={currentItem.name}
             onChange={(e) => newItem(e)}
-        />
+          />
+          <i className="fa-solid fa-cart-shopping fa-lg icon"></i>
+        </div>
+        
         <ul>
           {allItems}
         </ul>
